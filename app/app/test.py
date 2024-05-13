4a52f645b8fb7b101c2d1386635a680b09ee446b
belloibrahv <belloibrahv@gmail.com>
2024-05-13 09:24:31 +0100

    Setting up Database (using Postgresql)

--- app/app/test.py
+++ app/app/test.py
@@ -2,11 +2,15 @@ from django.test import SimpleTestCase
 from app.calc import add_two_numbers
 
 
-class CalcTest(SimpleTestCase):
-    "Test calc module"
+class CalcTestView(SimpleTestCase):
+    """Test calc module"""
 
     def test_calc_two_numbers(self):
+        """Add two numbers together"""
         res = add_two_numbers(10, 11)
-        expected = 21
 
-        self.assertEqual(res, expected)
+        self.assertEqual(res, 21)
+
+    def test_calc_subtract(self):
+        """Subtract two number"""
+        res = self
