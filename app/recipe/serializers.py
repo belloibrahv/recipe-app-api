91368fdae347e1be7fbac55fcdf693535b76b59c
belloibrahv <belloibrahv@gmail.com>
2024-05-24 13:23:21 +0100

    Added tags field (to the Recipe API)

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,21 +1,54 @@
-"""
-Serializers for recipe APIs
-"""
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import Recipe, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializers for recipes."""
+    """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title',
-                  'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
+        fields = ('id', 'title', 'time_minutes', 'price', 'link', 'tags')
+        read_only_fields = ('id',)
+
+    def _get_or_create_tag(self, tags, recipe):
+        """Handle getting and creating tags as needed."""
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=self.context['request'].user, **tag,)
+            recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+
+        self._get_or_create_tag(tags, recipe)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', [])
+        instance = super().update(instance, validated_data)
+        instance.tags.clear()
+
+        self._get_or_create_tag(tags, instance)
+
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ('description',)
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,13 +2,14 @@
 URL mappings for recipe app.
 """
 from django.urls import (path, include)
-from recipe.views import RecipeViewSet
+from recipe.views import (RecipeViewSet, TagViewSet)
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
 router.register('recipes', RecipeViewSet)
+router.register('tags', TagViewSet)
 
 app_name = 'recipe'
 
