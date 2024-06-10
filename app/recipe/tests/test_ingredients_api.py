db48d8243b16a035f8653292b8f6cba458d866a3
belloibrahv <belloibrahv@gmail.com>
2024-06-10 16:29:56 +0100

    Added filter feature (to the recipe API)

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -1,6 +1,7 @@
 """
 Tests for the ingredients API.
 """
+from decimal import Decimal
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -8,7 +9,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Ingredient
+from core.models import Ingredient, Recipe
 
 from recipe.serializers import IngredientSerializer
 
@@ -97,3 +98,48 @@ class PrivateIngredientApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         ingredients = Ingredient.objects.filter(user=self.user)
         self.assertFalse(ingredients.exists())
+
+    def test_filter_ingredient_assigned_to_recipe(self):
+        """Test listing ingredients by those assigned to recipe."""
+        ing1 = Ingredient.objects.create(user=self.user, name='Apples')
+        ing2 = Ingredient.objects.create(user=self.user, name='Turkey')
+        recipe = Recipe.objects.create(
+            user=self.user,
+            title='Apple Crumble',
+            time_minutes=5,
+            price=Decimal('9.00')
+        )
+        recipe.ingredients.add(ing1)
+
+        res = self.client.get(INGREDIENT_URL, {'assigned_only': 1})
+
+        s1 = IngredientSerializer(ing1)
+        s2 = IngredientSerializer(ing2)
+
+        self.assertIn(s1.data, res.data)
+        self.assertNotIn(s2.data, res.data)
+
+    def test_filtered_ingredients_unique(self):
+        """Test filtered ingredient returns a unique list."""
+        ing = Ingredient.objects.create(user=self.user, name='Eggs')
+        Ingredient.objects.create(user=self.user, name='Lentils')
+
+        recipe1 = Recipe.objects.create(
+            user=self.user,
+            title='Egg Benedict',
+            time_minutes=50,
+            price=Decimal('4.99')
+        )
+
+        recipe2 = Recipe.objects.create(
+            user=self.user,
+            title='Herb Egg',
+            time_minutes=20,
+            price=Decimal('4.00')
+        )
+        recipe1.ingredients.add(ing)
+        recipe2.ingredients.add(ing)
+
+        res = self.client.get(INGREDIENT_URL, {'assigned_only': 1})
+
+        self.assertEqual(len(res.data), 1)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,15 @@
 """
 Views for the recipe APIs.
 """
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+from functools import reduce
+import operator
+from django.db.models import Q, Count
 from rest_framework import (viewsets, mixins, status)
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -11,6 +20,22 @@ from core.models import (Recipe, Tag, Ingredient)
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of IDs to filter.',
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of IDs to filter.',
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
@@ -18,9 +43,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_integer(self, qs):
+        """Convert a list of string to integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve recipes for authenticated users."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_integer(tags)
+            tag_queries = [Q(tags__id=id) for id in tag_ids]
+            queryset = queryset.filter(reduce(operator.or_, tag_queries))
+        if ingredients:
+            ingredient_ids = self._params_to_integer(ingredients)
+            ingredient_queries = [Q(ingredients__id=id)
+                                  for id in ingredient_ids]
+            queryset = queryset.filter(
+                reduce(operator.or_, ingredient_queries))
+        return queryset.filter(user=self.request.user).order_by('-id').distinct()
 
     def get_serializer_class(self):
         """Return serializer class for request."""
@@ -48,17 +90,37 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            )
+        ]
+    )
+)
 class BaseRecipeAtrrViewSet(mixins.ListModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.DestroyModelMixin,
                             viewsets.GenericViewSet):
+    """Base viewset for authenticated attributes."""
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False).annotate(
+                num_recipes=Count('recipe')).filter(num_recipes__gt=0)
+        else:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset.order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAtrrViewSet):
